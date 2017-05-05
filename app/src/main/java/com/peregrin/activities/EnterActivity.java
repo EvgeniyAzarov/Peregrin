27375d1b906c9af72937fb328c680349e4bee49b
Vadym <vadym2312@gmail.com>
2017-05-05 20:58:09 +0300

    Enter Activity

--- app/src/main/java/com/peregrin/activities/EnterActivity.java
+++ app/src/main/java/com/peregrin/activities/EnterActivity.java
@@ -1,15 +1,44 @@
 package com.peregrin.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.peregrin.R;
 
+import es.dmoral.toasty.Toasty;
+
 public class EnterActivity extends AppCompatActivity {
+    EditText user_phone;
+    EditText user_password;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+
+        user_phone = (EditText) findViewById(R.id.enter_phone);
+        user_password = (EditText) findViewById(R.id.enter_password);
+        bt = (Button) findViewById(R.id.enter_bt);
+
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(checkEnterParameters(user_phone.getText().toString(), user_password.getText().toString())){
+                    Intent intent = new Intent(EnterActivity.this, ChatActivity.class);
+                    EnterActivity.this.startActivity(intent);
+                }
+                else{
+                    Toasty.warning(EnterActivity.this,getString(R.string.wrong_password)).show();
+                }
+            }
+        });
+    }
+    boolean checkEnterParameters(String phone, String password){
+        return true;
     }
 }
